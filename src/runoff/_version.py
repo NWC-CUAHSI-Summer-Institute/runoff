e16b534bf88ad88a9ae2757bf7a4cb9cce126821
Leo Lonzarich <lgl5139@psu.edu>
2026-07-19 11:45:37 -0500

    linting

--- src/runoff/_version.py
+++ src/runoff/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev20+g9beeae1e3.d20260719'
-__version_tuple__ = version_tuple = (0, 1, 'dev20', 'g9beeae1e3.d20260719')
+__version__ = version = '0.1.dev26+g97a10b24c.d20260719'
+__version_tuple__ = version_tuple = (0, 1, 'dev26', 'g97a10b24c.d20260719')
 
 __commit_id__ = commit_id = None
